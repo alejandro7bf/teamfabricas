4bc73a46d57855a2facbeea73578f55e4911c3f6
Alejandro Barrera <alejandro.barrera.sj@gmail.com>
2021-06-14 21:21:07 -0500

    Firebase Initalization and structure layout

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:teamfabricas/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(TeamFabricasApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
